4d830636556e088557586f5bf646bf7f616060ca
2018wzh <wzh20070720@outlook.com>
2026-07-22 04:03:18 +0800

    fix(emu): stabilize family plugin feature identity

--- Emulator/Source/Families/astra-emu-minori/build.rs
+++ Emulator/Source/Families/astra-emu-minori/build.rs
@@ -34,7 +34,7 @@ fn main() {
 
     let mut features = env::vars()
         .filter_map(|(name, _)| name.strip_prefix("CARGO_FEATURE_").map(str::to_owned))
-        .filter(|name| name != "DYNAMIC_PLUGIN_EXPORT")
+        .filter(|name| !matches!(name.as_str(), "DEFAULT" | "DYNAMIC_PLUGIN_EXPORT"))
         .collect::<Vec<_>>();
     features.sort();
     let feature_identity = format!(
